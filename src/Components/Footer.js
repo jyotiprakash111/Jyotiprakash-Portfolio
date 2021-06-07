c5b2207b42f68034fb9a64428d5e48fe7cac36dc
Jyotiprakash Sethy <3561167-Jyotiprakash1@users.noreply.gitlab.com>
2021-06-07 16:46:35 +0530

    Modified

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
 
     if(this.props.data){
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
       })
     }
 
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2021 Unique Media</li>
+              <li>Designed by <a title="Styleshout" href="https://twitter.com/Jyotiprakassh">Jyotiprakash</a></li>
            </ul>
 
         </div>
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,14 +30,14 @@ class Portfolio extends Component {
 
         <div className="row">
 
-          <div className="twelve columns collapsed">
+          {/* <div className="twelve columns collapsed">
 
             <h1>Check Out Some of My Works.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-              {/* {projects} */}
+              {projects}
             </div>
-          </div>
+          </div> */}
         </div>
       </section>
     );
