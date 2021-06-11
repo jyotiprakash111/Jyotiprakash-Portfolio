b221c1b83763b6806b922606d50475aaca2c917d
Jyotiprakash Sethy <3561167-Jyotiprakash1@users.noreply.gitlab.com>
2021-06-11 21:28:31 +0530

    Whatsapp_DirectChat

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -23,7 +23,7 @@ class Contact extends Component {
           </div>
 
           <div className='ten columns'>
-            <p className='lead'>{message}</p>
+            <p className='lead'></p>
           </div>
         </div>
 
